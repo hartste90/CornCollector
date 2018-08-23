042446760c7910b56d81699ff28e150e5cbc2ec3
Steve Hart <steveh@01421-mbp-260.local>
2018-08-23 13:34:44 -0700

    Coins now accelerate towards player

--- Assets/Scripts/GravitateToPlayer.cs
+++ Assets/Scripts/GravitateToPlayer.cs
@@ -7,20 +7,28 @@ public class GravitateToPlayer : MonoBehaviour {
     public float gravityDistance = 10f;
     public float gravityMagnitude = 5f;
 
+    public float speed = 0f;
+    public float acceleration = .1f;
+    public bool isFollowing = true;
+
  	private Transform playerTransform;
+     private Rigidbody2D rb;
 
     void Start()
     {
     	playerTransform = GameObject.FindGameObjectWithTag("Player").transform;
+        rb = GetComponent<Rigidbody2D>();
     }
     void Update () 
     {
-        float distance = Vector3.Distance(transform.position, playerTransform.position);
-        // Debug.Log(distance);
-        if (distance < gravityDistance)
-        {
-            transform.position = Vector3.Lerp(transform.position, playerTransform.position, gravityMagnitude * Time.deltaTime);
-        }
+        AccellerateTowardsTarget();
     }
+
+    private void AccellerateTowardsTarget()
+    {
+        Vector3 direction = playerTransform.position - transform.position;
+        rb.AddForce(new Vector2(direction.x, direction.y).normalized * acceleration, ForceMode2D.Impulse);
+    }
+    
 
  }
