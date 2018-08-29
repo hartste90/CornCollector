99f2f9bf03990344c810eb9c010546a0e1028089
Steve Hart <steveh@01421-mbp-260.local>
2018-08-29 16:52:42 -0700

    Added playing rewarded video ads to continue

--- Assets/Scripts/EndgameScreenController.cs
+++ Assets/Scripts/EndgameScreenController.cs
@@ -1,5 +1,6 @@
 ﻿using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.Advertisements;
 
 public class EndgameScreenController : MonoBehaviour {
 
@@ -7,11 +8,23 @@ public class EndgameScreenController : MonoBehaviour {
     public Text bestCoinCount;
     public Text totalCoinCount;
 
+    public AdController adController;
+
+    public Button continueButton;
+
     public void PopulateEndgameScreenContent(string recentCoinCountSet, string bestCoinCountSet, string totalCoinCountSet)
 	{
         this.recentCoinCount.text = recentCoinCountSet;
         this.bestCoinCount.text = bestCoinCountSet;
         this.totalCoinCount.text = totalCoinCountSet;
+        if (adController.IsReady())
+        {
+            continueButton.gameObject.SetActive(true);
+        }
+    }
 
-	}
+    public void HandleContinueButtonPressed()
+    {
+        adController.ShowRewardedAd();
+    }
 }
