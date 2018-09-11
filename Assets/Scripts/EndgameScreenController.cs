fce22df3d47a3a14c4429e74da1885044748652b
Steve Hart <steveh@01421-mbp-260.local>
2018-09-11 15:03:27 -0700

    Upgrading end game screen

--- Assets/Scripts/EndgameScreenController.cs
+++ Assets/Scripts/EndgameScreenController.cs
@@ -4,43 +4,65 @@ using UnityEngine.Advertisements;
 
 public class EndgameScreenController : MonoBehaviour {
 
-	public Text recentCoinCount;
-    public Text bestCoinCount;
-    public Text totalCoinCount;
+	public Text recentCoinCountText;
+    public Text bestCoinCountText;
+    public Text totalCoinCountText;
+    public Text continueCoinCostText;
+    public Text coinsNeededText;
+
 
     public GameController gameController;
     public AdController adController;
     public GameObject gameOverPanel;
-    public GameObject getCoinsPanel;
+    public GameObject storePanel;
+    public GameObject goToStorePanel;
 
     public Button continueAdButton;
-    public Button continueCoinButton;
+    public GameObject continueCoinButton;
 
-	public void ShowEndGameScreen()
+    private int continueCoinCost;
+    private int recentCoinCount;
+    private int bestCoinCount;
+    private int totalCoinCount;
+
+
+    public void PopulateEndgameScreenContent(string recentCoinCountSet, string bestCoinCountSet, string totalCoinCountSet)
 	{
-        getCoinsPanel.SetActive(false);
+        this.recentCoinCount = System.Int32.Parse(recentCoinCountSet); ;
+        this.bestCoinCount = System.Int32.Parse(bestCoinCountSet); ;
+        this.totalCoinCount = System.Int32.Parse(totalCoinCountSet);
+        this.recentCoinCountText.text = recentCoinCountSet;
+        this.bestCoinCountText.text = bestCoinCountSet;
+        this.totalCoinCountText.text = totalCoinCountSet;
+        this.continueCoinCost = Mathf.Max(200, (((System.Int32.Parse(recentCoinCountSet) / 2) / 10) * 10));
+        this.continueCoinCostText.text = "-"+this.continueCoinCost.ToString();
+
+        this.coinsNeededText.text = (this.continueCoinCost - this.recentCoinCount).ToString();
+
+    }
+
+    public void ShowEndGameScreen()
+    {
+        storePanel.SetActive(false);
         gameOverPanel.SetActive(true);
 
         //TODO animate UI on
-        if (adController.IsReady())
+        if (adController.IsReady()) //play an ad
         {
             continueAdButton.gameObject.SetActive(true);
             continueCoinButton.gameObject.SetActive(false);
         }
-        else
+        else if (this.continueCoinCost > this.recentCoinCount) // not enough coins, show buy button
+        {
+            goToStorePanel.SetActive(true);
+        }
+        else    //show coin continue button
         {
             continueAdButton.gameObject.SetActive(false);
             continueCoinButton.gameObject.SetActive(true);
         }
     }
 
-    public void PopulateEndgameScreenContent(string recentCoinCountSet, string bestCoinCountSet, string totalCoinCountSet)
-	{
-        this.recentCoinCount.text = recentCoinCountSet;
-        this.bestCoinCount.text = bestCoinCountSet;
-        this.totalCoinCount.text = totalCoinCountSet;
-
-    }
 
     public void HandleContinueAdButtonPressed()
     {
@@ -49,13 +71,19 @@ public class EndgameScreenController : MonoBehaviour {
 
     public void HandleContinueCoinButtonPressed()
     {
-        gameController.ContinueGame(200);
+        gameController.ContinueGame(this.continueCoinCost);
 
     }
 
-    public void HandleBuyCoinButtonPressed()
+    public void HandleGetCoinButtonPressed()
     {
-        //TODO: go to buy coin screen
+        storePanel.SetActive(true);
+        gameOverPanel.SetActive(false);
+    }
+
+    public void HandleBuyCoinButtonPressed(int packageId)
+    {
+        IAPManager.PurchasePackage(packageId);
     }
 
     public void HandleStartOverButtonPressed()
