4b2604529577cbf0fa8abe65f821f1b8f5dac314
Steve Hart <steveh@01421-mbp-260.local>
2018-11-18 20:06:37 -0800

    Adding title screen

---
+++ Assets/Rotate.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rotate : MonoBehaviour {
+
+    public float multiplier = .1f;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        float rotAmt = Mathf.Sin(Time.time);
+
+        transform.Rotate(Vector3.forward, rotAmt * multiplier);
+	}
+}
